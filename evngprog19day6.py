b79aa4e6f2300fc908fd212c97f1beff2cae7d32
NBAREERA <121182362+NBAREERA@users.noreply.github.com>
2022-12-28 09:48:53 +0530

    Add files via upload

---
+++ evngprog19day6.py
@@ -0,0 +1,10 @@
+string='''practice problems for list com pre hension in python.'''
+ans=[]
+wordsList=string.split(' ')
+for i in wordsList:
+     if len(i)<5:
+       ans.append(i)
+print(*ans)
+
+ans=[i for i in wordsList if len(i)<5]
+print(*ans)
