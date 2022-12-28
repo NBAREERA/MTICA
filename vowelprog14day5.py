56943b357aaf6fa73bd5fd4f225db7ab58a719a2
NBAREERA <121182362+NBAREERA@users.noreply.github.com>
2022-12-28 09:46:12 +0530

    Add files via upload

---
+++ vowelprog14day5.py
@@ -0,0 +1,11 @@
+def extract_vowel(s):
+    temp_vowel=''
+    for i in s:
+        if i in 'AEIOUaeiou':
+            temp_vowel+=i
+            print("i:",i,"temp_vowel:",temp_vowel)
+    return temp_vowel
+str1=input("enter the string")
+str1=input()
+a=extract_vowel(str1)
+print("vowel:",a)
