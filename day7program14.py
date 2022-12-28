e558063819ca118149ca3317e3284a69acb6b3ac
NBAREERA <121182362+NBAREERA@users.noreply.github.com>
2022-12-28 16:52:52 +0530

    Add files via upload

---
+++ day7program14.py
@@ -0,0 +1,8 @@
+'''Lst=[10,15,20,25,30,35,40,45]
+12)use list comprehension to construct a new list but add 6 to each item.
+'''
+Lst=[10,15,20,25,30,35,40,45]
+ans=[i+6 for i in Lst]
+#for i in Lst:
+    #ans.append(i+6)
+print(ans)
