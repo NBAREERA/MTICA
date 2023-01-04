70fb9a19b50ee1dab779467dfabb4d0ea94f1af4
NBAREERA <121182362+NBAREERA@users.noreply.github.com>
2023-01-04 17:41:57 +0530

    Add files via upload

--- day9prog2seriesstar.py
+++ day9prog2seriesstar.py
@@ -1,9 +1,15 @@
-def printSeries(ch,n):
-    sp='.'
-    for i in range(0,n):
-        print(sp*(n-i-1)+ch*(2*i+1)+sp*(n-i-1))
-    return None
-inpch=input()
-inpNum=int(input())
-printSeries(inpch,inpNum)
-    
+##def printSeries(ch,n):
+##    sp='.'
+##    for i in range(0,n):
+##        print(sp*(n-i-1)+ch*(2*i+1)+sp*(n-i-1))
+##    return None
+##inpch=input()
+##inpNum=int(input())
+##printSeries(inpch,inpNum)
+##    
+n=int(input())
+if n<0:
+    print('INVALID')
+else:
+    for i in range(n):
+        print('.'*(n-i-1)+'*'*((i*2)+1)+'.'*(n-i-1))
